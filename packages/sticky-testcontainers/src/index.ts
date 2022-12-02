fdc54a2cae8de1daa19c826dd8155f48ce1a3fc5
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2022-12-02 17:05:30 +0800

    feat(testcontainers): Remove LocalstackProContainer

--- packages/sticky-testcontainers/src/index.ts
+++ packages/sticky-testcontainers/src/index.ts
@@ -1,6 +1,5 @@
 export * from './GenericContainer';
 export * from './LocalstackContainer';
-export * from './LocalstackProContainer';
 export * from './PostgreSqlContainer';
 export * from './RedisContainer';
 export type { StoppedTestContainer } from 'testcontainers';
