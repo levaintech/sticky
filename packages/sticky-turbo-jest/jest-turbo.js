3557ebd2db7176b717465b764251e803491e4f3f
Fuxing Loh <git@fuxing.dev>
2023-05-31 11:33:04 +0800

    feat(sticky-turbo): support extends on turbo.json feat(sticky-jest-turbo): detect turbo is the current process to prevent re-invocation chore(*): fix wrong lint script
    This PR contains breaking changes when `sticky-turbo` is used programmatically.

--- packages/sticky-turbo-jest/jest-turbo.js
+++ packages/sticky-turbo-jest/jest-turbo.js
@@ -1,41 +1,24 @@
-const { spawnSync } = require('node:child_process');
-const { existsSync } = require('node:fs');
-const { join } = require('node:path');
-const { TurboJson, PackageJson } = require('@birthdayresearch/sticky-turbo');
+const { Turbo } = require('@birthdayresearch/sticky-turbo');
 
 module.exports = async function (_, project) {
-  const turboJson = new TurboJson(project.rootDir);
-  const packageJson = new PackageJson(project.rootDir);
-  const displayName = project.displayName.name;
-
-  for (const script of turboJson.getPipeline(displayName)?.dependsOn ?? []) {
-    if (script.startsWith('^')) {
-      await run(turboJson.getRootDir(), script.substring(1), `${packageJson.getName()}^...`);
-    } else if (packageJson.hasScript(script)) {
-      await run(turboJson.getRootDir(), script, packageJson.getName());
-    }
+  if (isTurbo()) {
+    console.log('jest-turbo: turbo is already running. skipping...');
+    return;
   }
+
+  const turbo = new Turbo(project.rootDir);
+  // project.displayName represent the script
+  const script = project.displayName.name;
+  turbo.runBefore(script, {
+    'output-logs': 'new-only',
+  });
 };
 
-async function run(rootDir, script, filter) {
-  const bin = './node_modules/.bin/turbo';
-  const args = ['run', script, `--filter=${filter}`, `--output-logs=new-only`];
-
-  if (!existsSync(join(rootDir, bin))) {
-    throw new Error(`Cannot find ${bin}`);
-  }
-
-  const spawn = spawnSync(bin, args, {
-    stdio: ['inherit', 'inherit', 'pipe'],
-    cwd: rootDir,
-  });
-
-  // Throw error if non-zero exit code encountered
-  if (spawn.status !== 0) {
-    const failureMessage =
-      spawn.stderr && spawn.stderr.length > 0
-        ? spawn.stderr.toString('utf-8')
-        : `Encountered non-zero exit code while running script: ${script}`;
-    throw new Error(failureMessage);
-  }
+/**
+ * Detects if the current process is a turbo invocation.
+ * If so, we don't need to run turbo again.
+ * @return {boolean}
+ */
+function isTurbo() {
+  return process.env.TURBO_INVOCATION_DIR !== undefined;
 }
---
+++ packages/sticky-turbo/src/Turbo.unit.ts
@@ -0,0 +1,41 @@
+import * as process from 'process';
+
+import { Turbo } from './Turbo';
+
+it(`turbo.runTask('invalid-script') should be ignored; porting a desired behavior that was present in <1.8.0`, async () => {
+  const turbo = new Turbo(process.cwd());
+  turbo.runTask('invalid-script', {
+    filter: `@levain-dev/sticky-turbo^...`,
+  });
+});
+
+it(`turbo.runTasks('invalid-script') should be ignored; porting a desired behavior that was present in <1.8.0`, async () => {
+  const turbo = new Turbo(process.cwd());
+  turbo.runTasks([
+    {
+      task: 'invalid-script',
+      opts: {
+        filter: `@levain-dev/sticky-turbo^...`,
+      },
+    },
+  ]);
+});
+
+it(`should turbo.runBefore('build') without failure`, async () => {
+  const turbo = new Turbo(process.cwd());
+  turbo.runBefore('build', {
+    dry: 'json',
+  });
+});
+
+it(`turbo.planPackages('test') should generate a list of packages to run`, async () => {
+  const turbo = new Turbo(process.cwd());
+  const packages = turbo.planPackages('test');
+  expect(packages).toStrictEqual(
+    expect.arrayContaining([
+      '@birthdayresearch/sticky-jest',
+      '@birthdayresearch/sticky-testcontainers',
+      '@birthdayresearch/sticky-turbo',
+    ]),
+  );
+});
