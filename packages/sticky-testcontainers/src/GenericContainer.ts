7d10125af8ae1267c606f4082a42e7fee0977de3
Fuxing Loh <git@fuxing.dev>
2023-06-09 14:09:39 +0800

    fix types

--- packages/sticky-testcontainers/src/GenericContainer.ts
+++ packages/sticky-testcontainers/src/GenericContainer.ts
@@ -1,18 +1,10 @@
 import { randomUUID } from 'node:crypto';
 
-import { GenericContainer as AbstractGenericContainer } from 'testcontainers';
-import { AbstractStartedContainer } from 'testcontainers/dist/modules/abstract-started-container';
+import { AbstractStartedContainer,GenericContainer as AbstractGenericContainer } from 'testcontainers';
 
 export type { StartedTestContainer } from 'testcontainers';
 
 export class GenericContainer extends AbstractGenericContainer {
-  protected override name: string = `artemys-generic-container-${randomUUID()}`;
-
-  public override withName(name: string): this {
-    this.name = name;
-    return this;
-  }
-
   constructor(image: string) {
     super(image);
     this.withName(`${image.replaceAll(/[^a-zA-Z0-9.-]/g, '-')}-${randomUUID()}`);
