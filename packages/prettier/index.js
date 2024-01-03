b5950e2d148f4a791c764a10035655ea41b3416d
Fuxing Loh <git@fuxing.dev>
2024-01-03 14:16:46 +0800

    change to import

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('prettier').Config} */
+const config = {
   printWidth: 120,
   trailingComma: 'all',
   singleQuote: true,
-  plugins: [require('prettier-plugin-tailwindcss'), require('prettier-plugin-packagejson')],
+  plugins: [import('prettier-plugin-tailwindcss'), import('prettier-plugin-packagejson')],
 };
+
+module.exports = config;
